778880959b978b96a11c763125a345515c955dd1
Stepan Vana <stepanvana10@gmail.com>
2022-06-08 21:19:11 +0200

    adding files

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <React.Fragment>
+            <h1>
+                About
+            </h1>
+            <p>
+                THis is the todo list app v1.0.0.
+            </p>
+        </React.Fragment>
+    )
+}
